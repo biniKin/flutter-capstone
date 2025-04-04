c46db3193b05d92b9776fb957e3c36977846af71
Biniyam Kinfe <biniyamkinfe21@gmail.com>
2025-04-04 06:17:33 -0700

    adding auth and storage

--- lib/features/core/data/service/api_service.dart
+++ lib/features/core/data/service/api_service.dart
@@ -1,1 +1,22 @@
-class ApiService {}
+import 'dart:convert';
+
+import 'package:capstone_project/features/core/data/models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  Future<List<ProductModel>> fetchProduct() async {
+    try {
+      final response = await http.get(
+        Uri.parse('https://fakestoreapi.com/products'),
+      );
+      if (response.statusCode == 200) {
+        List<dynamic> jsonData = jsonDecode(response.body);
+        return jsonData.map((item) => ProductModel.fromJson(item)).toList();
+      } else {
+        throw Exception('');
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
--- lib/features/core/data/service/auth_service.dart
+++ lib/features/core/data/service/auth_service.dart
@@ -1,1 +1,26 @@
-class AuthService {}
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  Future<User?> signIn(String password, String email) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      throw Exception('');
+    }
+  }
+
+  Future<User?> login(String email, String password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      return userCredential.user;
+    } catch (e) {
+      throw Exception('');
+    }
+  }
+
+  void signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+}
