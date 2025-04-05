464f68eb351a03f5e524b4222e176da0e2266ac8
Meron Getaneh <124381940+meronheron@users.noreply.github.com>
2025-04-05 16:20:19 +0300

    Create wishlist.dart

---
+++ lib/features/core/domain/entities/wishlist.dart
@@ -0,0 +1,11 @@
+class Wishlist {
+  final String id;
+  final String userId;
+  final List<Product> products;
+
+  Wishlist({
+    required this.id,
+    required this.userId,
+    required this.products,
+  });
+}
