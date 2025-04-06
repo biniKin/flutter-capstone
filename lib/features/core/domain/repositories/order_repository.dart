dbaf5614a52967afd7c6886e8a7faf55ca7c78b5
Meron Getaneh <124381940+meronheron@users.noreply.github.com>
2025-04-06 13:20:45 +0300

    Update order_repository.dart

--- lib/features/core/domain/repositories/order_repository.dart
+++ lib/features/core/domain/repositories/order_repository.dart
@@ -1,4 +1,4 @@
-import 'package:capstone_project/features/core/domain/entities/order_repository.dart
+import 'package:capstone_project/features/core/domain/entities/order_repository.dart'
 abstract class OrderRepository {
   Future<List<OrderItem>> getOrderItems(String userId, String orderId);
   Future<void> createOrder(String userId, List<CartItem> cartItems);
